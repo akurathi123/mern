b64b0b12e0feb1372960a061c6a3117c94d3a4dc
akurathi123 <akurathiyasaswinsai@gmail.com>
2026-04-17 16:41:15 +0530

    updated project files

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"https://mern-hhad.onrender.com"
+    baseURL:"https://mern-hhad.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
